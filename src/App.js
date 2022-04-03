fde6a55c1482e4c533727654c1b85a29e404d312
DeAnne Lombardi <64880045+themockingjedi@users.noreply.github.com>
2022-04-02 21:48:49 -0400

    cleaned up some files and overall structure

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       <h1>React Wk 4 Homework</h1>
       <h2>Migrating the thing that can do stuff</h2>
       <Search />
+      <footer>
+        This app was written by DeAnne Lombardi and is{" "}
+        <a
+          href="https://github.com/themockingjedi/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
